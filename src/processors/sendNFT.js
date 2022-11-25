619c31dbafbb71e99d545767dd23ce9055dda254
sruthi23 <sruthi@lightrains.com>
2022-11-25 15:02:50 +0530

    Add contract utils to mintNFt

--- src/processors/sendNFT.js
+++ src/processors/sendNFT.js
@@ -1,7 +1,15 @@
 'use strict'
 
+//const { mintNFT } = require('../utils/contract')
 module.exports = function (args, done) {
-  const { wallet, email, userId, name, affiliateCode } = args.data
+  const { wallet,affiliateCode,redis } = args.data
   console.log(args.data)
+  try {
+    await mintNFT(wallet)
+    redis.incrBy(`${affiliateCode}`, -1)
+
+  } catch (error) {
+      throw error
+  }
   done()
 }
