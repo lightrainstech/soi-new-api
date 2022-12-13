088e16475979249b43a1f2014f7f8987a935b498
Athul <a.athuljoy@gmail.com>
2022-12-13 10:02:57 +0530

    Remove unused api and imports

--- src/services/user.js
+++ src/services/user.js
@@ -1,5 +1,5 @@
 'use strict'
-const crypto = require('crypto')
+
 const ethUtil = require('ethereumjs-util')
 
 const User = require('../models/userModel.js')
@@ -17,6 +17,8 @@ const EXPIRESIN = process.env.JWT_TOKEN_EXPIRY || '3d'
 
 module.exports = async function (fastify, opts) {
   let { redis } = fastify
+
+  // User sign up
   fastify.post(
     '/signup',
     { schema: userPayload.otpSchema },
@@ -38,14 +40,12 @@ module.exports = async function (fastify, opts) {
           return reply
         }
         if (user === null) {
-          const otp = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000
           userModal.name = name
           userModal.userName = userName
           userModal.phone = phone
           userModal.email = email
           userModal.country = country
           userModal.wallet = await checkSumAddress(wallet)
-          userModal.otp = otp
 
           if (affCode) {
             userModal.role = 'influencer'
@@ -92,122 +92,41 @@ module.exports = async function (fastify, opts) {
         reply.error({ message: `Something went wrong: ${error}` })
       }
     }
-  ),
-    fastify.post(
-      '/otpresend',
-      { schema: userPayload.otpResendSchema },
-      async function (request, reply) {
-        const otp = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000
-        const { phone, country } = request.body
-        let user = await userModal.resetOtp(otp, phone, country)
-        if (user === null) {
-          reply.error({ message: 'No such user exists, please sign up.' })
-        } else {
-          reply.success({
-            message: 'New OTP has been sent successfully',
-            otp: otp
-          })
-        }
-      }
-    ),
-    fastify.post(
-      '/otpverify',
-      { schema: userPayload.otpVerifySchema },
-      async function (request, reply) {
-        const { phone, country, otp } = request.body
-        let user = await userModal.verifyOtp(otp, phone, country)
-        if (user === null) {
-          reply.error({ message: 'OTP is invalid or already verified' })
-        } else {
-          const accessToken = fastify.jwt.sign(
-            { userId: user._id, isVerified: user.isVerified },
-            { expiresIn: EXPIRESIN }
-          )
-          reply.success({
-            message: 'OTP has been verified successfully',
-            accessToken: accessToken
-          })
-        }
-      }
-    ),
-    fastify.post(
-      '/login',
-      { schema: userPayload.loginSchema },
-      async function (request, reply) {
-        const { password } = request.body,
-          email = request.body.email.toString().toLowerCase()
+  )
+  // Get logged in user details
+  fastify.get(
+    '/me',
+    { schema: userPayload.getMeSchema, onRequest: [fastify.authenticate] },
+    async function (request, reply) {
+      reply.success({
+        message: 'Success'
+      })
+    }
+  )
 
-        await userModal
-          .getActiveUserByEmail(email)
-          .then(async user => {
-            if (!user) {
-              reply.error({
-                message: 'No such account found or account is restricted!'
-              })
-            } else {
-              const isAuth = user.authenticate(password, user.hashed_password)
-              if (isAuth) {
-                const jwt = fastify.jwt.sign(
-                  {
-                    userId: user._id,
-                    name: user.name
-                  },
-                  { expiresIn: EXPIRESIN }
-                )
-                let respUser = {
-                  userId: user._id,
-                  name: user.name,
-                  accessToken: jwt
-                }
-                reply.code(200).success(respUser)
-              } else {
-                reply.error({
-                  message: 'Invalid email or password'
-                })
-              }
-            }
-          })
-          .catch(function (err) {
-            console.log(err)
-            reply.error({
-              message: 'Something went wrong',
-              data: err
-            })
-          })
-
-        return reply
-      }
-    ),
-    fastify.get(
-      '/me',
-      { schema: userPayload.getMeSchema, onRequest: [fastify.authenticate] },
-      async function (request, reply) {
+  // Get available nft details
+  fastify.get(
+    '/availableNft',
+    { schema: userPayload.nftAvailableSchema },
+    async function (request, reply) {
+      const { affCode } = request.query
+      try {
+        console.log('affCode', affCode)
+        let count = (await redis.get(`NFTC:${affCode}`)) || 0
+        console.log('###', count)
         reply.success({
-          message: 'Success'
+          message: 'Remaining NFts',
+          data: Number(count)
+        })
+      } catch (error) {
+        reply.error({
+          message: error
         })
       }
-    ),
-    fastify.get(
-      '/availableNft',
-      { schema: userPayload.nftAvailableSchema },
-      async function (request, reply) {
-        const { affCode } = request.query
-        try {
-          console.log('affCode', affCode)
-          let count = (await redis.get(`NFTC:${affCode}`)) || 0
-          console.log('###', count)
-          reply.success({
-            message: 'Remaining NFts',
-            data: Number(count)
-          })
-        } catch (error) {
-          reply.error({
-            message: error
-          })
-        }
-        return reply
-      }
-    ),
+      return reply
+    }
+  ),
+    // Wallet signature verification
     fastify.post(
       '/walletConnect',
       { schema: userPayload.walletConnectSchema },
