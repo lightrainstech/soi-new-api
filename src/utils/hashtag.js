cb7532315c13dab1e2084973b1c7176907ef6c29
Athul <a.athuljoy@gmail.com>
2023-02-22 12:26:48 +0530

    Update  function generate random hashTag

--- src/utils/hashtag.js
+++ src/utils/hashtag.js
@@ -1,7 +1,22 @@
-const { customAlphabet } = require('nanoid')
-const randomHashTag = customAlphabet(
-  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-  3
-)
+'use strict'
 
-module.exports = {randomHashTag}
+const randomHashTag = () => {
+  const characters =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const strings = new Set()
+
+  while (strings.size < characters.length ** 3) {
+    let string = ''
+    for (let i = 0; i < 3; i++) {
+      string += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    if (!strings.has(string)) {
+      strings.add(string)
+    }
+  }
+
+  const randomIndex = Math.floor(Math.random() * strings.size)
+  return Array.from(strings)[randomIndex]
+}
+
+module.exports = { randomHashTag }
