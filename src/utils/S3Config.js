728951d112bec7ab61e8a976d742a47ee0335a7d
Athul <a.athuljoy@gmail.com>
2023-02-28 12:27:42 +0530

    Update S3 config

--- src/utils/S3Config.js
+++ src/utils/S3Config.js
@@ -8,4 +8,28 @@ S3.config.update({
   region: process.env.S3_REGION
 })
 
-module.exports = S3
+// Upload file
+const uploadToS3 = async (file, key) => {
+  const fileName = file[0].filename.replace(/[^a-zA-Z0-9.]/g, '')
+  const uniqFileName = `${Date.now()}-${fileName}`,
+    fileDirPath = `${key}/${uniqFileName}`
+
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: fileDirPath,
+      Body: file[0].data,
+      ContentType: file[0].mimetype,
+      ACL: 'public-read'
+    }
+
+    // Upload file to S3
+    const upload = await S3.upload(params).promise()
+
+    // Return file url
+    return { link: upload.Location }
+}
+
+module.exports = {
+  S3,
+  uploadToS3
+}
